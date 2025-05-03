f00f2f98bf0b0594237a1249607739f9af376c13
Antonio Ramos Gonzalez <gonzalez372576@uabc.edu.mx>
2025-05-02 17:57:32 -0700

    Login

--- front/src/Views/login.jsx
+++ front/src/Views/login.jsx
@@ -1,1 +1,34 @@
 import React from "react";
+
+export default function Login() {
+  return (
+    <div className="flex items-center justify-center  h-screen">
+      <div className="card">
+        <div className="card-body items-center text-center bg-amber-200">
+          <h2 className="card-title">Login</h2>
+          <label className="floating-label">
+            <input
+              type="text"
+              className="input input-md"
+              required
+              placeholder="username or email"
+            />
+            <span className="floating-label">user or email</span>
+          </label>
+
+          <label className="floating-label">
+            <input
+              type="password"
+              className="input input-md"
+              required
+              placeholder="password"
+            />
+            <span className="floating-label">Password</span>
+          </label>  
+
+          <button className="btn btn-neutral btn-outline">Login</button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,18 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router'
-import './index.css'
-import App from './App.jsx'
-import Ransomware from './Views/Ransomware.jsx'
-import Home from './Views/home.jsx'
-import InformativePage from './Views/informativePage.jsx'
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import "./index.css";
+import App from "./App.jsx";
+import Ransomware from "./Views/Ransomware.jsx";
+import Home from "./Views/home.jsx";
+import InformativePage from "./Views/informativePage.jsx";
+import Login from "./Views/login.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/ramsomware" element={<InformativePage />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
