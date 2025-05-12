001a102d80901c8d2e668c4dbdd4c158fe2efd26
Antonio Ramos Gonzalez <gonzalez372576@uabc.edu.mx>
2025-05-12 08:34:46 -0700

    add fonts and little changes in home and infCard

--- front/src/Views/home.jsx
+++ front/src/Views/home.jsx
@@ -4,6 +4,9 @@ import PruebaConexion from "./prueba";
 import InformativeCards from "../components/informativeCards";
 import LetterGlitch from "../Backgrounds/letterGlitch/LetterGlitch";
 import DecryptedText from "../TextAnimations/DecryptedText/DecryptedText";
+
+import Virus from "../assets/images/virus.png";
+
 export default function Home() {
   return (
     <>
@@ -25,9 +28,9 @@ export default function Home() {
               <DecryptedText
                 text={"Bienvenido a"}
                 speed={100}
-                fontSize={"text-7xl"}
+                fontSize={"text-9xl"}
                 animateOn="view"
-                className="text-[#D9FAE7]"
+                className="text-[#D9FAE7] text-8xl"
                 sequential={true}
                 encryptedClassName="text-[#D9FAE7]"
               />
@@ -35,29 +38,22 @@ export default function Home() {
               <DecryptedText
                 text={"CiberShield"}
                 speed={100}
-                fontSize={"text-7xl"}
+                fontSize={""}
                 animateOn="view"
-                className="text-[#C3ACD5]"
+                className="text-[#C3ACD5] text-8xl"
                 sequential={true}
                 encryptedClassName="text-[#C3ACD5]"
               />
             </h1>
-          </div>
-          <div className=" card bg-[var(--colorHomeCard)] row-span-2 text-white w-[80%] h-[100%] shadow-xl">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime quo
-            ullam doloremque totam nostrum iusto ab! Mollitia blanditiis dolores
-            natus sapiente? Adipisci cupiditate voluptate tenetur quisquam odit,
-            est nisi autem?
+            <div className=" card bg-[var(--olo)] row-span-2 text-white w-[80%]  shadow-xl"></div>
           </div>
 
-          <div className="bg-amber-200 breack-words">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis
-            similique iusto officiis voluptas! Illo, repudiandae porro.
-            Cupiditate pariatur eveniet nemo aliquam fugit accusamus corporis
-            dicta nulla enim fuga, facere magni?
+          <div className="text-white text-4xl font-Base ">
+            Tu escudo contra las amenazas digitales
           </div>
-
-          <div className="card bg-[#9090AB] col-span-2 row-span-3  w-full  ">
+          {/* #e5f5ff */}
+          {/* #ccebfe */}
+          <div className="card bg-[#e5f5ff] col-span-2 row-span-3 w-full h-full ">
             <div className="flex flex-col items-center h-full">
               <h2 className=" text-5xl font-bold">SERVICIOS</h2>
               <div className="grid grid-cols-3 gap-4 mt-4 mb-4">
@@ -68,6 +64,7 @@ export default function Home() {
                   }
                   color={"#FF4848"}
                   link={"/ransomware"}
+                  image={Virus}
                 />
                 <InformativeCards
                   title={"Ingenieria social"}
--- front/src/components/informativeCards.jsx
+++ front/src/components/informativeCards.jsx
@@ -1,30 +1,44 @@
 import React from "react";
 import { NavLink } from "react-router";
-import Arrow from "../assets/arrow-right.png"
+import Arrow from "../assets/arrow-right.png";
 
-export default function InformativeCards({title,description,color,image,link}) {
-    return (
-        <>
-            <div className="card bg-[var(--tempColor)] w-96 shadow-lg" style={{
-                '--tempColor': color,
-            }}>
-                <h1 className="card-title text-3xl font-bold text-center mb-4">
-                 {title}
-                </h1>
-                <figure className="px-10 pt-10">
-                    <img src={image} ></img>
-                </figure>
-                <div className="card-body">
-                    <p>{description}</p>
-                    <div className="card-actions justify-center mt-4">
-                        <NavLink to={link} className="btn bg-[var(--colorLight)] text-[var(--colorDark)] hover:bg-[var(--colorDark)] hover:text-[var(--colorLight)]">
-                            <p>Saber mas</p>
-                            <img src={Arrow} alt="Arrow" className="w-6 h-6" />
-                        </NavLink>
-                    </div>  
-                </div>
+export default function InformativeCards({
+  title,
+  description,
+  color,
+  image,
+  link,
+}) {
+  return (
+    <>
+      <div
+        className="card w-96 shadow-xl rounded-lg overflow-hidden transform transition duration-300 hover:scale-105"
+        style={{ "--tempColor": color }}
+      >
+        <div className="bg-[var(--tempColor)] p-6 text-center">
+          <h1 className="text-3xl font-bold text-white mb-4">{title}</h1>
+          <figure className="flex justify-center">
+            <img
+              src={image}
+              alt={`${title} image`}
+              className="max-h-40 object-contain"
+            />
+          </figure>
+        </div>
 
-            </div>
-        </>
-    )
+        <div className="card-body p-6 bg-white">
+          <p className="text-gray-700">{description}</p>
+          <div className="card-actions justify-center mt-6">
+            <NavLink
+              to={link}
+              className="flex items-center gap-2 btn px-4 py-2 rounded-md bg-[var(--colorLight)] text-[var(--colorDark)] hover:bg-[var(--colorDark)] hover:text-[var(--colorLight)] transition"
+            >
+              <span>Saber más</span>
+              <img src={Arrow} alt="Arrow icon" className="w-5 h-5" />
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
