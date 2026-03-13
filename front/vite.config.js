0782099412f1fc9835a435a9fa61d86e7f21825b
Antonio Ramos Gonzalez <gonzalez372576@uabc.edu.mx>
2026-03-13 15:14:21 -0700

    lectura del .env

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,14 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import taiwindcss from '@tailwindcss/vite'
-// https://vite.dev/config
-export default defineConfig({
-  plugins: [react(),taiwindcss()],
-  proxy: {
-    '/api': {
-      target: 'http://localhost:3000',
-      changeOrigin: true,
-      rewrite: (path) => path.replace(/^\/api/, '')
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig(({ mode }) => {
+
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.BACK_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
+      }
     }
-  },
+  }
 })
